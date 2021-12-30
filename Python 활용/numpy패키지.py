8535085c1ca3686e8ed21f5210078eafc24843f4
RiGun-K <1234suho@naver.com>
2021-12-30 13:06:42 +0900

    numpy 패키지

--- Python 활용/numpy패키지.py
+++ Python 활용/numpy패키지.py
@@ -1,7 +1,11 @@
 # 다차원 배열을 처리하는데 필요한 기능을 제공 (머신러닝, 데이터 분석 등)
 
+# 터미널에 pip install matplotlib, numpy 등을 입력하여 설치를 해야한다. 
+# 경로 C:\Users\RiGun\Python>
+
 import numpy as np
 
 list1 = [1,2,3,4]
 a = np.array(list1)
 print(a.shape) # (4, )
+
