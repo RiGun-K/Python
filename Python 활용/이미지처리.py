471ca5933216f7c6e2ba67e6b91b45cf91327421
RiGun-K <1234suho@naver.com>
2022-01-04 17:10:54 +0900

    2022/01/04

---
+++ Python 활용/이미지처리.py
@@ -0,0 +1,17 @@
+# pillow - 이미징 라이브러리
+
+import os
+import cv2
+
+from PIL import Image
+
+# 이미지 열기
+im = Image.open('C:\Users\RiGun\Python\Python 활용\Profile.jpg')
+
+im.show()
+
+# 이미지 크기 출력
+print(im.size)
+
+# 이미지 PNG로 저장
+im.save('Profile.png')
