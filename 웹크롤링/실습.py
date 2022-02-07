0a90609cad808fa297d177bab3ae92f2d2fb781f
RiGun-K <1234suho@naver.com>
2022-02-07 19:14:30 +0900

    네이버 API 활용 실습

--- 웹크롤링/실습.py
+++ 웹크롤링/실습.py
@@ -33,7 +33,7 @@ def getRequestUrl(url):
     
 #[CODE 2]
 # 데이터 가져올 API 주소 설정
-def getNaverSearch(node, srcText, start, display):
+def getNaverSearch(node, srcText, start, display):  
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
     parameters = "?query=%s&start=%s&display=%s"%(urllib.parse.quote(srcText), start, display)
@@ -53,16 +53,16 @@ def getNaverSearch(node, srcText, start, display):
 
 # getPostData() - 응답 데이터를 정리하여 리스트에 저장하기
 def getPostData(post, jsonResult, cnt):
-    title = post['title']
-    description = post['description']
+    title = post['title']   # post 객체의 title 항목에 저장된 값
+    description = post['description']   
     org_link = post['originallink']
     link = post['link']
 
     # 조회한 실시간 날짜 설정
-    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
-    pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')  # 문자열을 날짜 객체 형식으로 변환
+    pDate = pDate.strftime('%Y-%m-%d %H:%M:%S') # 날짜 객체의 표시 형식을 지정
 
-    # JSON으로 가져올 데이터 설정
+    # 리스트 객체인 jsonResult에 원소를 추가 
     jsonResult.append({'cnt':cnt, 'title':title, 'description':description, 'org_link':org_link, 'link':org_link, 'pDate':pDate})
     return
 
@@ -70,28 +70,30 @@ def getPostData(post, jsonResult, cnt):
 # 터미널 상에서 조회후 JSON으로 파일 생성
 def main():
     node = 'news' #크롤링할 대상
-    srcText = input('조회할 검색어를 입력하세요: ')
-    cnt = 0
-    jsonResult = []
-    jsonResponse = getNaverSearch(node, srcText, 1, 100) #[CODE 2]
-    total = jsonResponse['total']
+    srcText = input('조회할 검색어를 입력하세요: ') # 사용자 입력으로 받은 검색어 저장
+    cnt = 0 # 검색 결과 카운트
+    jsonResult = [] # 결과를 저장할 리스트 객체
+    jsonResponse = getNaverSearch(node, srcText, 1, 100) # 1부터 100개의 검색 결과를 처리 [CODE 2] 
+    total = jsonResponse['total']   # 전체 검색 결과수 = 네이버 뉴스 검색에 대한 응답을 저장하는 객체
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
-            cnt += 1
+            cnt += 1    # 1개 씩 증가 하도록
             getPostData(post, jsonResult, cnt) #[CODE 3]
 
+        # getNaverSearch() 호출하여 새 결과를 저장하고 for 문 다시 반복
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) #[CODE 2]
 
     print('전체 검색 : %d 건' %total)
 
     with open('%s_naver_%s.json' % (srcText, node), 'w', encoding='utf8') as outfile:
+        # jsonFile = JSON 파일에 저장할 데이터를 담은 객체
         jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True, ensure_ascii = False)
         # json.dumps() - 리스트를 JSON 파일로 저장하기
          
         outfile.write(jsonFile)
 
-    print("가져온 데이터 : %d 건" %(cnt))
+    print("가져온 데이터 : %d 건" %(cnt))   
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
 if __name__ == '__main__':
