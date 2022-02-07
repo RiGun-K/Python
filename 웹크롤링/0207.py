c2d4fbf56d1ca0cc74fb3da78445ef8f63f94412
RiGun-K <1234suho@naver.com>
2022-02-07 11:43:42 +0900

    네이버 크롤링

--- 웹크롤링/0207.py
+++ 웹크롤링/0207.py
@@ -1,5 +1,9 @@
 # 네이버 검색 API예제는 블로그를 비롯 전문자료까지 호출방법이 동일하므로 blog검색만 대표로 예제를 올렸습니다.
 # 네이버 검색 Open API 예제 - 블로그 검색
+
+# 터미널 상에서 json으로 데이터 출력하는 방법 
+
+
 import os
 import sys
 import urllib.request
--- 웹크롤링/실습.py
+++ 웹크롤링/실습.py
@@ -7,7 +7,15 @@ import json
 client_id = ''
 client_secret = ''
 
+
+# 터미널 상에서 원하는 검색어 입력하면 데이터 조회후 같은경로에서 JSON 파일(데이터 파일) 생성 
+    # 1. 검색어 지정
+    # 2. 네이버 뉴스 검색   ( getNaverSearch() , getRequestUrl() )
+    # 3. 응답 데이터 정리 후 리스트에 저장 ( getPostData() )
+    # 4. 리스트를 JSON 파일로 저장
+
 #[CODE 1]
+# API 접속 설정
 def getRequestUrl(url):
     req = urllib.request.Request(url)
     req.add_header("X-Naver-Client-Id", client_id)
@@ -24,36 +32,45 @@ def getRequestUrl(url):
         return None
     
 #[CODE 2]
+# 데이터 가져올 API 주소 설정
 def getNaverSearch(node, srcText, start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
     parameters = "?query=%s&start=%s&display=%s"%(urllib.parse.quote(srcText), start, display)
 
+    # url 구성
     url = base + node + parameters
     responseDecode = getRequestUrl(url) #[CODE 1]
 
     if (responseDecode == None):
         return None
     else:
+        # 요청 결과를 JSON으로 받기 
         return json.loads(responseDecode)
 
 #[CODE 3]
+# 조회할 데이터 설정
+
+# getPostData() - 응답 데이터를 정리하여 리스트에 저장하기
 def getPostData(post, jsonResult, cnt):
     title = post['title']
     description = post['description']
     org_link = post['originallink']
     link = post['link']
 
+    # 조회한 실시간 날짜 설정
     pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
     pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
 
-    jsonResult.append({'cnt':cnt, 'title':title, 'description': description, 'org_link':org_link, 'link': org_link, 'pDate':pDate})
+    # JSON으로 가져올 데이터 설정
+    jsonResult.append({'cnt':cnt, 'title':title, 'description':description, 'org_link':org_link, 'link':org_link, 'pDate':pDate})
     return
 
 #[CODE 0]
+# 터미널 상에서 조회후 JSON으로 파일 생성
 def main():
     node = 'news' #크롤링할 대상
-    srcText = input('검색어를 입력하세요: ')
+    srcText = input('조회할 검색어를 입력하세요: ')
     cnt = 0
     jsonResult = []
     jsonResponse = getNaverSearch(node, srcText, 1, 100) #[CODE 2]
@@ -69,9 +86,9 @@ def main():
     print('전체 검색 : %d 건' %total)
 
     with open('%s_naver_%s.json' % (srcText, node), 'w', encoding='utf8') as outfile:
-        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
-                                     ensure_ascii = False)
-
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True, ensure_ascii = False)
+        # json.dumps() - 리스트를 JSON 파일로 저장하기
+         
         outfile.write(jsonFile)
 
     print("가져온 데이터 : %d 건" %(cnt))
