b1700ad39e4459ade363bdb3fa9dc54572699c40
RiGun-K <1234suho@naver.com>
2022-02-10 14:47:39 +0900

    2022/02/10

--- TensorFlow-DNN 실습/Deep_NN.py
+++ TensorFlow-DNN 실습/Deep_NN.py
@@ -3,9 +3,13 @@ tf.disable_v2_behavior()
 
 import numpy as np
 
+# 털과 날개가 있는지 없는지에 따라, <포유류인지 조류인지> 분류하는 신경망 모델을 만드는 실습
+# 신경망의 레이어를 여러개로 구성하여 딥러닝 구성
+
+    # [털, 날개]
 x_data = np.array(
     [[0,0],[1,0],[1,1],[0,0],[0,0],[0,1]])
-
+    # [기타, 포유류, 조류]
 y_data = np.array([
     [1,0,0], # 기타
     [0,1,0], # 포유류
@@ -15,48 +19,61 @@ y_data = np.array([
     [0,0,1]
 ])
 
-#######
-# 신경망 모델 구성
-#######
+###################
+# 신경망 모델 구성 #
+###################
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
+# W1의 열(10) 과 W2의 행(10) 이 같아야 함 !! 
+    # 첫번째 가중치의 차원은 [특성, 히든 레이어의 뉴런갯수] -> [2, 10] 으로 정함
 W1 = tf.Variable(tf.random_uniform([2, 10], -1., 1.))
+    # 두번째 가중치의 차원을 [첫번째 히든 레이어의 뉴런 갯수, 분류 갯수] -> [10, 3] 으로 정함
 W2 = tf.Variable(tf.random_uniform([10, 3], -1., 1.))
 
+    # 편향을 각각 각 레이어의 아웃풋 갯수로 설정
+    # b1 은 히든 레이어의 뉴런 갯수로, b2 는 최종 결과값 즉, 분류 갯수인 3으로 설정
 b1 = tf.Variable(tf.zeros([10]))
 b2 = tf.Variable(tf.zeros([3]))
 
+    # 신경망의 히든 레이어에 가중치 W1과 편향 b1을 적용
 L1 = tf.add(tf.matmul(X, W1), b1)
 L1 = tf.nn.relu(L1)
 
+    # 최종적인 아웃풋을 계산
+    # 히든레이어에 두번째 가중치 W2 와 편향 b2를 적용하여 3개의 출력값 생성
 model = tf.add(tf.matmul(L1, W2), b2)
 
+    # 텐서플로우에서 기본적으로 제공되는 크로스 엔트로피 함수 사용
+    # 복잡한 수식을 사용하지 않고도 최적화를 위한 비용 함수를 아래와 같이 적용
 cost = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y, logits=model))
 
+    # Optimizer를 통해 오차범위를 줄어들도록 설정
 optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
 train_op = optimizer.minimize(cost)
 
-############
-# 신경망 모델 학습
-############
-
+###################
+# 신경망 모델 학습 #
+###################
+    
+    # 초기화
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+    # 100번 학습
 for step in range(100):
     sess.run(train_op, feed_dict={X: x_data, Y: y_data})
     
     if (step + 1) % 10 == 0:
         print(step + 1, sess.run(cost, feed_dict={X: x_data, Y: y_data}))
         
-########
+#############################
 # 결과 확인
-# 0: 기타 1: 포유류 2: 조류
-########
+# 0: 기타 1: 포유류 2: 조류 #
+#############################
 
 prediction = tf.argmax(model, 1)
 target = tf.argmax(Y, 1)
